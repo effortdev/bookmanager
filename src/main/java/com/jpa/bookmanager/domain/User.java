d0849430745218d4ac34a1b56b9374fcbfe5fb54
ktj <kindxowns@gmail.com>
2025-03-21 10:51:04 +0900

    error resolustion...

--- src/main/java/com/jpa/bookmanager/domain/User.java
+++ src/main/java/com/jpa/bookmanager/domain/User.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NonNull
--- src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -15,9 +18,10 @@ class UserRepositoryTest {
 
     @Test
     void crud(){
-        userRepository.save(new User());
+        List<User> users = userRepository.findAll();
 
-        userRepository.findAll().forEach(System.out::println);
+        users.forEach(System.out::println);
+
     }
 
 }
